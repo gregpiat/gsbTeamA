89efddac65dc341cfebd063d66c2a3587bf36287
gregpiat <gregoire.piat@icof.fr>
2014-11-13 16:45:25 +0100

    Ajout clotureFicheFrais

---
+++ clotureFicheFrais.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  $repInclude = './include/';
+  require($repInclude . "_init.inc.php");
+  require($repInclude . "_entete.inc.html");
+  require($repInclude . "_sommaire.inc.php");
+  
+  
+?>
